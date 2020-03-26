c53ba87093b118a6bb0c7e446e9b4ab3f2177f4b
jdo8351 <61482874+jdo8351@users.noreply.github.com>
2020-03-26 17:45:42 -0500

    spacing

--- js/script.js
+++ js/script.js
@@ -14,8 +14,3 @@ span.onclick = function() {
   modal.style.display = "none";
 };
 
-// window.onclick = function(event) {
-//   if (event.target === modal) {
-//     modal.style.display = "none";
-//   }
-// };
